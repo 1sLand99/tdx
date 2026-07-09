02a34ef45817cbf5e2162df646d2c7c379961c9c
钱纯净 <1113655791@qq.com>
2026-07-09 23:14:02 +0800

    fix(httpserver): update example to use Default() with new Option API

--- example/HTTPServer/main.go
+++ example/HTTPServer/main.go
@@ -1,1 +1,12 @@
-package HTTPServer
+package main
+
+import (
+	"github.com/injoyai/logs"
+	"github.com/injoyai/tdx/extend/httpserver"
+)
+
+func main() {
+	s, err := httpserver.Default()
+	logs.PanicErr(err)
+	s.Run()
+}
